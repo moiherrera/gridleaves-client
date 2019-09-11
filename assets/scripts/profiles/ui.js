1dc3a1696c38cfa36b8de0991068b01b07022520
moiherrera <moi.herrera@outlook.com>
2019-09-11 13:18:23 -0400

    Add UI features

--- assets/scripts/profiles/ui.js
+++ assets/scripts/profiles/ui.js
@@ -3,51 +3,48 @@ const showProfilesTemplate = require('../templates/profiles-page.handlebars')
 
 const onCreateSuccess = (formData) => {
   console.log(formData)
-  $('#message2').text('Success Profile Created')
+  $('#message2').text('Success Profile Created.')
   $('#create-profile').hide()
   $('form').trigger('reset')
-  console.log(formData)
-  console.log(formData.profiles)
   $('#gridleavesTable').show()
 }
 
 const onCreateFailure = (formData) => {
   $('#message2').text('Failure: Profile not Created')
 }
-
-const onIndexSuccess = (data) => {
-  console.log(data)
-  console.log(data.profiles)
-  const showProfilesHtml = showProfilesTemplate({profiles: data.profiles})
-  $('#create-profile').hide()
-  $('.content').html(showProfilesHtml)
-  $('.content').show()
-  $('#new-profile').on('click', onAddNewProfile)
-  $('#get-profiles').hide()
-  $('.input_class').hide()
-}
 const onUpdateSuccess = function () {
   $('#message2').text('Profile Updated')
 }
 
+const onIndexSuccess = (data) => {
+  const showProfilesHtml = showProfilesTemplate({profiles: data.profiles})
+  $('#create-profile').hide()
+  $('.content').html(showProfilesHtml)
+  $('.content').show()
+  $('#message3').text('Welcome to Gridleaves. Here are the Profiles')
+  $('#new-profile').on('click', onAddNewProfile)
+  $('#get-profiles').hide()
+  $('.input_class').hide()
+}
+
 const onAddNewProfile = function () {
-  console.log('hello')
   $('#content').hide()
   $('#create-profile').show()
+  $('#message2').text('Add a New Profile!')
+  $('#message3').text()
 }
-// }
+const onIndexFailure = function () {
+  $('#message2').text('No profiles to get!')
+}
 
-// const onIndexFailure
-//
-// const onUpdateSuccess
-//
-// const onUpdateFailure
-//
+const onUpdateFailure = function () {
+  $('#message2').text('Profile does not belong to user. Cannot Update')
+}
 const onDestroySuccess = function () {
   $('#message2').text('Success Profile Destroyed')
 }
 const onDestroyFailure = function () {
-  $('#message2').text('Failed to Destroy Profile')
+  $('#message2').text('Profile does not belong to user. Cannot Destroy')
 }
 //
 // const onDestroyFailure
@@ -59,5 +56,7 @@ module.exports = {
   onAddNewProfile,
   onDestroySuccess,
   onDestroyFailure,
-  onUpdateSuccess
+  onUpdateSuccess,
+  onIndexFailure,
+  onUpdateFailure
 }
